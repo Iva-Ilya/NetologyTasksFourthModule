6a8834dabe8350ef8a7a695bdeb8af9a6f308195
IlyaIvanov <ilya.ivanov.0605@gmail.com>
2026-03-22 11:54:18 +0300

    Delete folder test

--- test/test.cpp
+++
@@ -1,6 +0,0 @@
-//Текстовый файл для проверки автоматической кодировки в UTF8 при создании новых файлов в Visual studio.
-/*
-Интсрукция по настройке автоматической кодировки в UTF8:
-Для автоматического сохранения в UTF-8 всех новых файлов, содержащих символы, отличные от набора ASCII, 
-необходимо включить опцию - Tools > Options > Environment > Documents > Save documents as Unicode when data cannot be saved in codepage
-*/
